d142500fc4942e275f25d78ae800acd48c11ee37
inksne <debanyj23@gmail.com>
2024-12-14 12:52:21 +0300

    delete useless comments

--- main.py
+++ main.py
@@ -22,46 +22,10 @@ app = FastAPI(title='weather')
 
 logger = logging.getLogger(__name__)
 
-
-# class Coordinates(NamedTuple):
-#     latitude: float
-#     longitude: float
-
 class UserResponse(BaseModel):
     id: int
     email: Optional[str] = None
     username: str
-
-
-# async def get_coords(location: str):
-#     try:
-#         loc = Nominatim(user_agent="GetLoc")
-#         getLoc = await asyncio.to_thread(loc.geocode, location)
-#         if getLoc is None:
-#             raise CantGetCoordinates("Локация не найдена.")
-        
-#         latitude = getLoc.latitude
-#         longitude = getLoc.longitude
-#         if USE_ROUNDED_COORDS:
-#             latitude, longitude = map(lambda c: round(c, 1), [latitude, longitude])
-#         return Coordinates(latitude=latitude, longitude=longitude)
-#     except CantGetCoordinates:
-#         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail='Не удалось получить координаты.')
-
-# @app.get('/api/v1/get_weather')
-# async def get_weather_end(location: str):
-#     try:
-#         coordinates = await get_coords(location)
-#         logger.debug(f'Координаты: {coordinates}')
-        
-#         weather = await get_weather(coordinates)
-#         return {'location': location, 'weather': format_weather(weather)}
-#     except CantGetCoordinates as e:
-#         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
-#     except ApiServiceError as e:
-#         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
-#     except AttributeError as e:
-#         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
     
 
 @app.post('/register', response_model=UserResponse)
